bfd2097ba619e2001040dffdc044cf88091c4709
djy <137359049@qq.com>
2018-08-27 13:01:34 +0800

    1.Glide集成，使用okHttp加载

---
+++ x-baseproject/src/main/java/com/xia/baseproject/integration/glide/GlideConfiguration.java
@@ -0,0 +1,61 @@
+package com.xia.baseproject.integration.glide;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+import com.xia.baseproject.rxhttp.RestCreator;
+
+import java.io.InputStream;
+
+/**
+ * @author xia
+ * @date 2018/8/27.
+ */
+@GlideModule
+public class GlideConfiguration extends AppGlideModule {
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(RestCreator.getOkHttpClient()));
+    }
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        /*
+          DiskCacheStrategy.NONE： 表示不缓存任何内容。
+          DiskCacheStrategy.DATA： 表示只缓存原始图片。
+          DiskCacheStrategy.RESOURCE： 表示只缓存转换过后的图片。
+          DiskCacheStrategy.ALL ： 表示既缓存原始图片，也缓存转换过后的图片。
+          DiskCacheStrategy.AUTOMATIC： 表示让Glide根据图片资源智能地选择使用哪一种缓存策略（默认选项）。
+         */
+        builder.setDefaultRequestOptions(new RequestOptions()
+                .format(DecodeFormat.PREFER_RGB_565)
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE));
+        int memoryCacheSizeBytes = 1024 * 1024 * 20;
+        builder.setMemoryCache(new LruResourceCache(memoryCacheSizeBytes));
+        int bitmapPoolSizeBytes = 1024 * 1024 * 30;
+        builder.setBitmapPool(new LruBitmapPool(bitmapPoolSizeBytes));
+        int diskCacheSizeBytes = 1024 * 1024 * 100;
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, diskCacheSizeBytes));
+    }
+
+    /**
+     * @return 设置清单解析，设置为false，避免添加相同的modules两次
+     */
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+}
