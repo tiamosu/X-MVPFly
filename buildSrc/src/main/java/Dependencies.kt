093e7aaafdbcc819b62bf746d9f5616f6a9455c6
djy <137359049@qq.com>
2018-11-28 16:36:21 +0800

    1.版本升级至2.2.8

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -24,7 +24,7 @@ object Versions {
 object Publish {
     const val userOrg = "weixia" //bintray.com用户名
     const val groupId = "me.xia" //jcenter上的路径
-    const val publishVersion = "2.2.7" //版本号
+    const val publishVersion = "2.2.8" //版本号
     const val desc = "Oh hi, this is a nice description for a project, right?"
     const val website = "https://github.com/wexia/X-MVPFly"
     const val gitUrl = "https://github.com/wexia/X-MVPFly.git"
