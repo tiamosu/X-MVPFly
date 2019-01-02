68630e3411db40668d1abd08b2aa43b14e469587
djy <137359049@qq.com>
2019-01-02 10:50:51 +0800

    1.okhttp3升级

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -14,7 +14,7 @@ object Versions {
     const val support = "28.0.0"
     const val butterknife = "9.0.0-rc3"
     const val retrofit2 = "2.5.0"
-    const val okhttp3 = "3.12.0"
+    const val okhttp3 = "3.12.1"
     const val autodispose = "1.1.0"
     const val leakcanary = "1.6.2"
     const val glide = "4.8.0"
