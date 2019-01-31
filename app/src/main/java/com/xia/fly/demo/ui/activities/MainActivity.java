0e0763675cfd65e99febc5a9dce4ff393de67372
djy <137359049@qq.com>
2019-01-31 11:14:46 +0800

    1.测试

--- app/src/main/java/com/xia/fly/demo/ui/activities/MainActivity.java
+++ app/src/main/java/com/xia/fly/demo/ui/activities/MainActivity.java
@@ -1,13 +1,16 @@
 package com.xia.fly.demo.ui.activities;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.widget.EditText;
 
+import com.xia.fly.demo.R;
 import com.xia.fly.demo.ui.fragments.MainFragment;
 import com.xia.fly.ui.activities.ProxyActivity;
 import com.xia.fly.ui.fragments.SupportFragment;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.ContentFrameLayout;
 import me.yokeyword.fragmentation.anim.DefaultVerticalAnimator;
 import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
@@ -31,20 +34,28 @@ public class MainActivity extends ProxyActivity {
         return false;
     }
 
-//    @Override
-//    public int getLayoutId() {
-//        return R.layout.activity_main;
-//    }
-//
-//    @Override
-//    protected void loadProxyRootFragment(int proxyContainerId) {
-//        super.loadProxyRootFragment(R.id.main_container_fl);
-//    }
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void loadProxyRootFragment(int proxyContainerId) {
+        super.loadProxyRootFragment(R.id.main_container_fl);
+    }
 
     @NonNull
     @Override
     protected Class<? extends SupportFragment> setRootFragment() {
         return MainFragment.class;
+    }
+
+    @Override
+    public void initView() {
+        final ContentFrameLayout layout = getContainerLayout();
+        if (layout != null) {
+            layout.setBackgroundColor(Color.GRAY);
+        }
     }
 
     @Override
