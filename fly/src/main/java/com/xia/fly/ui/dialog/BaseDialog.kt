e93a772a241d9cc249f944da27222f7051073a36
djy <137359049@qq.com>
2019-03-07 14:31:40 +0800

    1.优化

--- fly/src/main/java/com/xia/fly/ui/dialog/BaseDialog.kt
+++ fly/src/main/java/com/xia/fly/ui/dialog/BaseDialog.kt
@@ -15,8 +15,7 @@ import com.xia.fly.R
  * @date 2018/7/29.
  */
 abstract class BaseDialog @JvmOverloads constructor(
-        context: Context, @StyleRes themeResId: Int = R.style.baseDialogStyle
-) : Dialog(context, themeResId) {
+        context: Context, @StyleRes themeResId: Int = R.style.baseDialogStyle) : Dialog(context, themeResId) {
 
     protected abstract fun getLayoutId(): Int
 
@@ -32,19 +31,18 @@ abstract class BaseDialog @JvmOverloads constructor(
 
     protected open fun initDialog() {
         setCanceledOnTouchOutside(false)
-        val window = window
-        if (window != null) {
-            val params = window.attributes
+        window?.apply {
+            val params = attributes
             params.width = (ScreenUtils.getScreenWidth() * 0.8).toInt()
             //dimAmount在0.0f和1.0f之间，0.0f完全不暗，即背景是可见的 ，1.0f时候，背景全部变黑暗。
             //如果要达到背景全部变暗的效果，需要设置  dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
             //否则，背景无效果。
             params.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND
-            window.attributes = params
+            attributes = params
             //这个函数用来设置 Dialog 周围的颜色。系统默认的是半透明的灰色。值设为0则为完全透明。
-            window.setDimAmount(0.2f)
-            window.setGravity(Gravity.CENTER)
-            window.setWindowAnimations(R.style.dialogEmptyAnimation)
+            setDimAmount(0.2f)
+            setGravity(Gravity.CENTER)
+            setWindowAnimations(R.style.dialogEmptyAnimation)
         }
     }
 
@@ -68,7 +66,6 @@ abstract class BaseDialog @JvmOverloads constructor(
                 }
             } catch (ignored: Exception) {
             }
-
         }
     }
 }
