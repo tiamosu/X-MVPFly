c644aa72dce03bdb8210380e9049b6fc52227fc6
djy <137359049@qq.com>
2019-06-21 08:09:00 +0800

    1.utilcode、dagger2、kotlin 升级

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -11,13 +11,13 @@ object Android {
 }
 
 object Versions {
-    const val kotlin = "1.3.31"
+    const val kotlin = "1.3.40"
     const val butterknife = "10.1.0"
     const val retrofit2 = "2.6.0"
     const val okhttp3 = "3.12.3" //版本3.12.X，兼容Android 4
     const val autodispose = "1.3.0"
     const val glide = "4.9.0"
-    const val dagger2 = "2.23.1"
+    const val dagger2 = "2.23.2"
     const val fragmentation = "1.1.1"
     const val fly = "2.8.8"
 }
@@ -76,7 +76,7 @@ object Deps {
     const val x_rxbus = "me.xia:x-flyrxbus:1.0.5"
 
     //utilcode
-    const val utilcode = "com.blankj:utilcodex:1.24.3"
+    const val utilcode = "com.blankj:utilcodex:1.24.4"
 
     //okhttp
     const val okhttp3 = "com.squareup.okhttp3:okhttp:${Versions.okhttp3}"
