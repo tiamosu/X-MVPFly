330fc7ee6f08bf148bfd0ec9fe1315d27fd20a37
djy <137359049@qq.com>
2019-07-04 09:14:34 +0800

    1.kotlin 升级

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -11,7 +11,7 @@ object Android {
 }
 
 object Versions {
-    const val kotlin = "1.3.40"
+    const val kotlin = "1.3.41"
     const val butterknife = "10.1.0"
     const val retrofit2 = "2.6.0"
     const val okhttp3 = "3.12.3" //版本3.12.X，兼容Android 4
